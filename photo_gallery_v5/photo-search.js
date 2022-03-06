4ac0b1d77bdca5dca40d0e468b2db0fa2f9f5038
webdevotter <webdevotter@gmail.com>
2022-03-06 17:56:16 -0600

    grid search layout

---
+++ photo_gallery_v5/photo-search.js
@@ -0,0 +1,15 @@
+// JavaScript Photo Search Function Code
+function search_photo() {
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('photos');
+      
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="grid";                 
+        }
+    }
+}
