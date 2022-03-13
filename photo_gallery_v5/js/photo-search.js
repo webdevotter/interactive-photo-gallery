a5a3bbc4093e33311c6c9bd8eb5f7b6d9f62d994
webdevotter <webdevotter@gmail.com>
2022-03-12 22:15:59 -0600

    updated responsive custom search function

--- photo_gallery_v5/js/photo-search.js
+++ photo_gallery_v5/js/photo-search.js
@@ -1,15 +1,24 @@
-// JavaScript Photo Search Function Code
-function search_photo() {
-    let input = document.getElementById('searchbar').value
-    input=input.toLowerCase();
-    let x = document.getElementsByClassName('photos');
+// Source: https://www.geeksforgeeks.org/search-bar-using-html-css-and-javascript/
+// Source: https://developer.mozilla.org/en-US/docs/Web/API/Element/getAttributeNode
+// Source: https://www.w3schools.com/jsref/met_element_getattribute.asp
 
+
+    let input = document.getElementById('searchbar'); //Takes user input
+    let x = document.querySelectorAll('a'); //pulls values from anchor tags containing data captions
+   
+    input.addEventListener("keyup", customSearch); // performs function below against user's input
+
+
+    
+    function customSearch() {
+    
     for (i = 0; i < x.length; i++) { 
-        if (!x[i].innerHTML.toLowerCase().includes(input)) {
-            x[i].style.display="none";
+        let j = x[i].getAttribute('data-caption').toLowerCase(); //convert data captions to lower case
+        if (j.includes(input.value.toLowerCase())){ //converts user's input to lower case and compares against data captions
+            x[i].style.display="grid"; //display images in grid where user's input matches data captions
         }
         else {
-            x[i].style.display="grid";                 
+            x[i].style.display="none";     // don't display if no matches            
         }
     }
 }
