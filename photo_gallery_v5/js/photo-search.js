1cc3587657224bec1e0f75a7d64b871ad662a9df
webdevotter <webdevotter@gmail.com>
2022-03-12 20:32:09 -0600

    responsive custom search

--- photo_gallery_v5/js/photo-search.js
+++ photo_gallery_v5/js/photo-search.js
@@ -3,7 +3,7 @@ function search_photo() {
     let input = document.getElementById('searchbar').value
     input=input.toLowerCase();
     let x = document.getElementsByClassName('photos');
-      
+
     for (i = 0; i < x.length; i++) { 
         if (!x[i].innerHTML.toLowerCase().includes(input)) {
             x[i].style.display="none";
